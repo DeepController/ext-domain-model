9c19e68423b2c39af6aeff2f9ce2cc09e33f09bc
Yang Wang <deepcontrolr@gmail.com>
2017-04-10 09:30:07 -0700

    Initial Commit

---
+++ ExtDomainModel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExtDomainModel
+//
+//  Created by William on 4/10/17.
+//  Copyright © 2017 Yang Wang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
